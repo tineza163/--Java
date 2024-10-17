2d8dd26086bd6996ef318523d6ae1a54af0c00df
tineza163 <86141438+tineza163@users.noreply.github.com>
2024-10-17 23:30:32 +0400

    Add files via upload

---
+++ TaskOne.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое целое число (a): ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе целое число (b): ");
+        int b = scanner.nextInt();
+
+        // Сравнение
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        // Арифметические операции
+        System.out.println("Сложение: " + (a + b));
+        System.out.println("Вычитание: " + (a - b));
+        System.out.println("Умножение: " + (a * b));
+        // Проверка деления на ноль
+        if (b != 0) {
+            System.out.println("Деление: " + (a / b));
+        } else {
+            System.out.println("Деление на ноль невозможно");
+        }
+
+        scanner.close();
+    }
+}
+
